6f3fff7a5f3f9490b0923015c8d16acb7ed33ef7
Swagat Mishra <swagatm@microsoft.com>
2019-06-26 16:55:17 -0700

    first commit

---
+++ 13.core-bot/MyTypingMiddleware.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+
+namespace CoreBot
+{
+    public class MyTypingMiddleware : IMiddleware
+    {
+        private readonly TimeSpan _delay;
+        private readonly TimeSpan _period;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShowTypingMiddleware"/> class.
+        /// </summary>
+        /// <param name="delay">Initial delay before sending first typing indicator. Defaults to 500ms.</param>
+        /// <param name="period">Rate at which additional typing indicators will be sent. Defaults to every 2000ms.</param>
+        public MyTypingMiddleware(int delay = 500, int period = 2000)
+        {
+            if (delay < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(delay), "Delay must be greater than or equal to zero");
+            }
+
+            if (period <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(period), "Repeat period must be greater than zero");
+            }
+
+            _delay = TimeSpan.FromMilliseconds(delay);
+            _period = TimeSpan.FromMilliseconds(period);
+        }
+
+        /// <summary>
+        /// Processess an incoming activity.
+        /// </summary>
+        /// <param name="turnContext">The context object for this turn.</param>
+        /// <param name="next">The delegate to call to continue the bot middleware pipeline.</param>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects
+        /// or threads to receive notice of cancellation.</param>
+        /// <returns>A task that represents the work queued to execute.</returns>
+        /// <remarks>Spawns a thread that sends the periodic typing activities until the turn ends.
+        /// </remarks>
+        /// <seealso cref="ITurnContext"/>
+        /// <seealso cref="Bot.Schema.IActivity"/>
+        public async Task OnTurnAsync(ITurnContext turnContext, NextDelegate next, CancellationToken cancellationToken)
+        {
+            CancellationTokenSource cts = null;
+            try
+            {
+                // If the incoming activity is a MessageActivity, start a timer to periodically send the typing activity
+                if (turnContext.Activity.Type == ActivityTypes.Message)
+                {
+                    cts = new CancellationTokenSource();
+                    cancellationToken.Register(() => cts.Cancel());
+
+                    // do not await task - we want this to run in thw background and we wil cancel it when its done
+                    var task = Task.Run(() => SendTypingAsync(turnContext, _delay, _period, cts.Token), cancellationToken);
+                }
+
+                await next(cancellationToken).ConfigureAwait(false);
+            }
+            finally
+            {
+                if (cts != null)
+                {
+                    cts.Cancel();
+                }
+            }
+        }
+
+        private static async Task SendTypingAsync(ITurnContext turnContext, TimeSpan delay, TimeSpan period, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await Task.Delay(delay, cancellationToken).ConfigureAwait(false);
+
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    if (!cancellationToken.IsCancellationRequested)
+                    {
+                        await SendTypingActivityAsync(turnContext, cancellationToken).ConfigureAwait(false);
+                    }
+
+                    // if we happen to cancel when in the delay we will get a TaskCanceledException
+                    await Task.Delay(period, cancellationToken).ConfigureAwait(false);
+                }
+            }
+            catch (TaskCanceledException)
+            {
+                // do nothing
+            }
+        }
+
+        private static async Task SendTypingActivityAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            // create a TypingActivity, associate it with the conversation and send immediately
+            var typingActivity = new Activity
+            {
+                Type = ActivityTypes.Typing,
+                RelatesTo = turnContext.Activity.RelatesTo,
+            };
+
+            // sending the Activity directly on the Adapter avoids other Middleware and avoids setting the Responded
+            // flag, however, this also requires that the conversation reference details are explicitly added.
+            var conversationReference = turnContext.Activity.GetConversationReference();
+            typingActivity.ApplyConversationReference(conversationReference);
+
+            // make sure to send the Activity directly on the Adapter rather than via the TurnContext
+            await turnContext.Adapter.SendActivitiesAsync(turnContext, new Activity[] { typingActivity }, cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
---
+++ 13.core-bot/Startup.cs
@@ -0,0 +1,187 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.BotBuilderSamples.Bots;
+using Microsoft.BotBuilderSamples.Dialogs;
+using Microsoft.Bot.Schema;
+using System;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using IMiddleware = Microsoft.Bot.Builder.IMiddleware;
+using CoreBot;
+
+namespace Microsoft.BotBuilderSamples
+{
+    public class Startup
+    {
+        public Startup()
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Create the credential provider to be used with the Bot Framework Adapter.
+            services.AddSingleton<ICredentialProvider, ConfigurationCredentialProvider>();
+
+            // Create the Bot Framework Adapter with error handling enabled.
+            //services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
+            services.AddSingleton<IBotFrameworkHttpAdapter, DefaultAdapter>();
+
+
+            // Create the storage we'll be using for User and Conversation state. (Memory is great for testing purposes.)
+            services.AddSingleton<IStorage, MemoryStorage>();
+
+            // Create the User state. (Used in this bot's Dialog implementation.)
+            services.AddSingleton<UserState>();
+
+            // Create the Conversation state. (Used by the Dialog system itself.)
+            services.AddSingleton<ConversationState>();
+
+            // The Dialog that will be run by the bot.
+            services.AddSingleton<MainDialog>();
+
+            // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
+            services.AddTransient<IBot, DialogAndWelcomeBot<MainDialog>>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            //app.UseHttpsRedirection();
+            app.UseWebSockets();
+            app.UseMvc();
+        }
+    }
+
+    //public class DefaultAdapter : AdapterBase
+    public class DefaultAdapter : BuggyAdapterBase
+    {
+        public DefaultAdapter(
+            ICredentialProvider credentialProvider)
+            : base(credentialProvider)
+        {
+            OnTurnError = async (turnContext, exception) =>
+            {
+                await turnContext.SendActivityAsync(new Activity(type: ActivityTypes.Trace, text: $"{exception.Message}"));
+                await turnContext.SendActivityAsync(new Activity(type: ActivityTypes.Trace, text: $"{exception.StackTrace}"));
+                await turnContext.SendActivityAsync("an Error Occurred");
+            };
+
+            Use(new MyTypingMiddleware());
+        }
+    }
+
+    public class AdapterBase : BotAdapter, IBotFrameworkHttpAdapter
+    {
+        private readonly BotFrameworkHttpAdapter _botFrameworkHttpAdapter;
+        private Lazy<bool> _ensureMiddlewareSet;
+        private readonly object initLock = new object();
+
+        private readonly List<Bot.Builder.IMiddleware> middlewares = new List<Bot.Builder.IMiddleware>();
+
+        public AdapterBase(ICredentialProvider credentialProvider = null, IChannelProvider channelProvider = null, ILoggerFactory loggerFactory = null)
+        {
+            _botFrameworkHttpAdapter = new BotFrameworkHttpAdapter(credentialProvider, channelProvider, loggerFactory?.CreateLogger<BotFrameworkHttpAdapter>());
+            _ensureMiddlewareSet = new Lazy<bool>(() =>
+            {
+                middlewares.ForEach(mw => _botFrameworkHttpAdapter.Use(mw));
+                return true;
+            });
+        }
+
+        public async Task ProcessAsync(HttpRequest httpRequest, HttpResponse httpResponse, IBot bot, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            bool _ = _ensureMiddlewareSet.Value;
+            await _botFrameworkHttpAdapter.ProcessAsync(httpRequest, httpResponse, bot, cancellationToken).ConfigureAwait(false);
+        }
+
+        public new AdapterBase Use(IMiddleware middleware)
+        {
+            lock (initLock)
+            {
+                middlewares.Add(middleware);
+            }
+            return this;
+        }
+
+        public override Task<ResourceResponse[]> SendActivitiesAsync(ITurnContext turnContext, Activity[] activities, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<ResourceResponse> UpdateActivityAsync(ITurnContext turnContext, Activity activity, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task DeleteActivityAsync(ITurnContext turnContext, ConversationReference reference, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class BuggyAdapterBase : BotAdapter, IBotFrameworkHttpAdapter
+    {
+        private readonly BotFrameworkHttpAdapter _botFrameworkHttpAdapter;
+        private Lazy<bool> _ensureMiddlewareSet;
+        private readonly object initLock = new object();
+
+        private readonly List<Bot.Builder.IMiddleware> middlewares = new List<Bot.Builder.IMiddleware>();
+
+        public BuggyAdapterBase(ICredentialProvider credentialProvider = null, IChannelProvider channelProvider = null, ILoggerFactory loggerFactory = null)
+        {
+            _botFrameworkHttpAdapter = new BotFrameworkHttpAdapter(credentialProvider, channelProvider, loggerFactory?.CreateLogger<BotFrameworkHttpAdapter>());
+            _ensureMiddlewareSet = new Lazy<bool>(() =>
+            {
+                _botFrameworkHttpAdapter.Use(base.MiddlewareSet);
+                return true;
+            });
+        }
+
+        public async Task ProcessAsync(HttpRequest httpRequest, HttpResponse httpResponse, IBot bot, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            bool _ = _ensureMiddlewareSet.Value;
+            await _botFrameworkHttpAdapter.ProcessAsync(httpRequest, httpResponse, bot, cancellationToken).ConfigureAwait(false);
+        }
+
+        public override Task<ResourceResponse[]> SendActivitiesAsync(ITurnContext turnContext, Activity[] activities, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<ResourceResponse> UpdateActivityAsync(ITurnContext turnContext, Activity activity, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task DeleteActivityAsync(ITurnContext turnContext, ConversationReference reference, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
